31e12da8e071d864a951abafdf312cb39a5067aa
mhdovicx <134880325+mhdovicx@users.noreply.github.com>
2023-06-23 16:19:34 +0100

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin
-#Copyright 2021 AllHackingTools
-#Written by : Misha Korzhik
-#Github     : http://github.com/mishakorzik
+#Copyright 2023 AllHackingTools and install tools
+#Written by : Mhdovic 
+#Github     : http://github.com/mhdovicx
 
 echo '
 [1]
@@ -12,8 +12,8 @@ echo '
    ██╔══██╗██║     ██╔══██║██║     ██╔═██╗ ██║███╗██║██║   ██║██║     ██╔══╝  
    ██████╔╝███████╗██║  ██║╚██████╗██║  ██╗╚███╔███╔╝╚██████╔╝███████╗██║     
    ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚══╝╚══╝  ╚═════╝ ╚══════╝╚═╝                                                                           
-[3] Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
-[3] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
-[2] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+[3] Title    : AllHackingTools - AllInstallTools  -  ⁣ATTENTION! The author of this article is not respo-
+[3] Github   : https://github.com/mhdovicx  -  nsi⁣ble responsible for any consequences of reading it. 
+[2] Coded By : Mhdovic-blackwolf  -  All materials are provided for educational purposes only! 
 [4]——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——[715]
 '|lolcat -p 1.6 -a -d 2 -s 20.0
